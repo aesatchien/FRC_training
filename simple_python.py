5e407c34087a9bd29da91439d8a79e9e3c3aced7
Hill <cjhill2@ndc.nasa.gov>
2022-06-17 21:46:10 -0700

    week 2 lectures

---
+++ simple_python.py
@@ -0,0 +1,10 @@
+# a very simple python script
+
+print('\n--Simple Calculator Script--')
+
+x = int(input('first number: '))
+y = int(input('second number: '))
+
+print(f'{x} plus {y} is {x+y}')
+
+print(f'')
