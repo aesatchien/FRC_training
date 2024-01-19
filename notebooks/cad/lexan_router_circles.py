5543f66b0c1027753337b859f97239dc575a5662
aesatchien <cjh123@yahoo.com>
2024-01-19 08:03:51 -0800

    updated standalone hole script for ease of use

--- notebooks/cad/lexan_router_circles.py
+++ notebooks/cad/lexan_router_circles.py
@@ -1,25 +1,26 @@
 # script you can run to make g-code for the router - 20240117 CJH
 # circles or center drill points
+# check your work by pasting to https://ncviewer.com/
 
 import simplegcode as sg
 import numpy as np
 
 # CHANGE PARAMETERS IN THIS SECTION - CHOOSE GRID OR PERIMETER
-output_file_description = 'test'
+output_file_description = 'test_circles'
 
 # simple way to generate a grid or perimeter - update these as needed
-perimeter_only = True
-x_start, x_final, x_spacing  = 0.5, 2.5, 1.0
-y_start, y_final, y_spacing  = 0.5, 4.5, 1.0
+perimeter_only = True  # forces entire grid or just the perimeter
+x_start, x_final, x_spacing = 0.5, 2.5, 1.0  # inches
+y_start, y_final, y_spacing = 0.5, 4.5, 1.0
 
 # update these as needed for the G-code instructions
 mill_circles = True  # false if you only want to drill
 depth = -0.26  # depth in Z for the operation (single step in this version)
 hole_diameter = (0.205-0.125)  # if you want to mill out a circle, subtract tool diameter manually here
 machine = 'shapeoko'  # shapeoko or benchmill, ON BENCHMILL YOU MAY NEED TO ADD A % FOR FANUC MODE
-display_plot = False  # show a plot of the points
+display_plot = False  # show a plot of the points after generating
 time_stamp = False  # timestamp the file name
-sweeps = 2  # number of inside sweeps of each circle
+sweeps = 2  # number of inside sweeps of each circle when cutting
 
 # -----------------------  do not modify beLow this line   -----------------------
 if perimeter_only:  # simple way to generate a perimeter - or any legs of a perimeter
@@ -40,5 +41,5 @@ code = sg.generate_gcode(point_list, machine=machine, peck=False, overrides=over
 
 print(f'Probably generated a file looking like {output_file_description}_shapeoko_circles.nc ...')
 
-if display_plot:
+if display_plot:  # put up a map of your points to double check
     sg.plot_points(point_list, annotate=False, show_order=True, interactive=False)
